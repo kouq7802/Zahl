987e9a52afe4176cf5786af002c04fd941c157a8
矢野　隆之 <takayuki@AZUSA-MacBook-Pro.local>
2016-01-20 18:12:37 +0900

    modify Repeating.Decimal.py

--- RepeatingDecimal.py
+++ RepeatingDecimal.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 '''
 Author: KouQ7802
@@ -12,27 +12,29 @@ import sys
 
 ## define functions
 def read_param():
+    ERROR_MESSAGE = '[Error] Program aborted!!\nPlease enter Natural Number'
     try:
         n = int(eval(sys.argv[1]))
         if n >= 0:
             return n
         else:
-            print('[Error] Program aborted!!')
-            print('Please enter natural number')
-            return None
+            raise ValueError
     except ValueError:
-        print('[Error] Program aborted!!')
-        print('Please enter natural number')
+        print(ERROR_MESSAGE)
+        sys.exit()
     except NameError:
-        print('[Error] Program aborted!!')
-        print('Please enter natural number')
+        print(ERROR_MESSAGE)
+        sys.exit()
+    except SyntaxError:
+        print(ERROR_MESSAGE)
+        sys.exit()
 
 def find_recurring_cycle(n):
     rabbit = 1
     turtle = 1
     s = 0
     g = 0
-    
+
     while True:
         rabbit = (rabbit * 10) % n
         rabbit = (rabbit * 10) % n
@@ -41,7 +43,7 @@ def find_recurring_cycle(n):
             break
         else:
             continue
-    
+
     if rabbit != 0:
         rabbit = 1
         s = 1
@@ -49,30 +51,34 @@ def find_recurring_cycle(n):
             s += 1
             turtle = (turtle * 10) % n
             rabbit = (rabbit * 10) % n
-        
+
         rabbit = (rabbit *10) % n
         g = s
-        
+
         while turtle != rabbit:
             g += 1
             rabbit = (rabbit * 10) % n
-        
-    return {'n':n, 's':s, 'g':g}
+
+    return {'n':n, 'startpoint':s, 'endpoint':g}
 
 def main():
     n = read_param()
-    if isinstance(n, int):
-        result = find_recurring_cycle(n)
-        print('='*36+'[RESULT]'+'='*36)
-        print(' 1/', n)
-        print('-'*80)
-        print(' length of recurring cycle      :', result['g']-result['s']+1)
-        print(' start point of recurring cycle :', result['s'])
-        print(' end point of recurring cycle   :', result['g'])
-        #print(result)
-        print('='*80)
+    result = find_recurring_cycle(n)
+    print('='*36+'[RESULT]'+'='*36)
+    print(' 1 /', n)
+    print('-'*80)
+    if result['startpoint'] == 0:
+        print(' Finite Decimal')
+    elif result['startpoint'] == 1:
+        print(' Pure Recurring Decimal')
     else:
-        pass
+        print(' Mixed Recurring Decimal')
+    print('-'*80)
+    print(' length of recurring cycle      :', result['endpoint']-result['startpoint']+1)
+    print(' start point of recurring cycle :', result['startpoint'])
+    print(' end point of recurring cycle   :', result['endpoint'])
+    #print(result)
+    print('='*80)
 
 if __name__ == '__main__':
     main()
